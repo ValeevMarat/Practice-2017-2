f0f44961691ecddc574eefb60726360113fbbd86
Marat Valeev <user@LAPTOP-7JKVIGGR>
2017-07-04 20:23:21 +0500

    Создан проект

--- Practice 2017 2/Program.cs
+++ Practice 2017 2/Program.cs
@@ -2,6 +2,10 @@
 
 namespace Practice_2017_2
 {
+    //Задание №2 практики 2017г.
+    //Создать алгоритм декодирования
+    //https://acmp.ru/index.asp?main=task&id_task=655
+
     class Program
     {
         static void Main(string[] args)
